e4fe8b63ceed6155c40b8dedaead569d00ec615c
TomJiX <47442725+TomJiX@users.noreply.github.com>
2020-03-12 04:10:18 +0100

    Add files via upload

---
+++ src/cluster.py
@@ -0,0 +1,30 @@
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+Data_3=[[10,10],[30,50],[20,10],[40,100]]
+
+
+class Cluster:
+    def __init__(self,obj_coor):
+        self.obj_coor=obj_coor
+    
+    def assign(self):
+        df = DataFrame(self.obj_coor)
+        kmeans = KMeans(n_clusters=2).fit(df)
+        labels=kmeans.labels_
+        drones=[[],[]]
+        for i in range (len(labels)):
+            drones[labels[i]].append(self.obj_coor[i])
+        return(drones)
+
+    def send_command(self):
+        obj=self.assign()
+        
+        for i in range(len(obj[0])):
+            print("go "+str(obj[0][i][0])+" "+str(obj[0][i][1]))
+        for j in range(len(obj[1])):
+            print("go "+str(obj[0][j][0])+" "+str(obj[0][j][1]))
+        
+
