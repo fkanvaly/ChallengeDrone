526dc78bb3a5b1926200b084bc7fb29cfadfab34
michelTho <thomazo.michel@gmail.com>
2020-03-12 03:07:48 +0100

    add a baseline in main

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from djitellopy import Tello
+from easyTello.easytello import Tello
+from time import sleep
+import cv2
+
+from mapping import Mapping
+
+
+HEIGHT = 50
+
 
 def main():
     
+    mapper = Mapping()
+    
     master_drone = Tello()
-    master_drone.connect()
     master_drone.takeoff()
     
-    master_drone.move_up(50)
+    sleep(3)
+    
+    master_drone.up(HEIGHT)
+    
+    sleep(3)
+    
     height = master_drone.get_height()
-     
+    print("la hauteur est de " + str(height))
+            
+    sleep(3)
+    
+    stream = master_drone.get_video_stream()
+    
+    sleep(3)
+    
+    map = mapper.map(stream, height)
+    
+    master_drone.land()
+    print(master_drone.get_battery())
+
+main()
+
